21b7d9475979f124ddaf311d7f631a43211c496b
Paola de Truchis <paola.truchis@gmail.com>
2023-11-22 16:49:45 -0600

    añadi comentarios para entender mejor lo que hace el codigo

--- EDP/part1.py
+++ EDP/part1.py
@@ -10,34 +10,48 @@ import numpy as np
 class ODEsolver(Sequential):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        # Inicialización del seguimiento de la pérdida
         self.loss_tracker = keras.metrics.Mean(name="loss")
+        # Uso del error cuadrático medio como función de pérdida
         self.mse = tf.keras.losses.MeanSquaredError()
 
+    # La linea siguiente permite definir un metedo que sea como un attribut 
     @property
+    # Este metodo permite enviar una lista que contiene las 'metrics' de self.loss_tracker
     def metrics(self):
       return [self.loss_tracker]
 
+
+    # Este funcion permite entrenar eln modelo
+    # Genera datos aleatorios para el entrenamiento, gracias a la biblioteca numpy
     def train_step(self, data):
+         
+         # obtencion del tamaño del batch
          batch_size = tf.shape(data)[0]
+         # Cálculo de los valores mínimos y máximos de los datos
+         # para que generemos valores dentro del rango de los datos
          min = tf.cast(tf.reduce_min(data),tf.float32)
          max = tf.cast(tf.reduce_max(data),tf.float32)
+         # Generamos valores aleatorios, gracias a numpy, a dentro del rango de los datos
          x = tf.random.uniform((batch_size,1), minval=min, maxval=max)
 
+         # Calculo del gradiente, con el modelo, para resolver la EDO
          with tf.GradientTape() as tape:
              with tf.GradientTape() as tape2:
                  tape2.watch(x)
+                 # Predicción (del modelo) de la fucion evaluada en 'x'
                  y_pred = self(x, training=True)
-             dy = tape2.gradient(y_pred, x) #derivada del modelo con respecto a entradas x
-             x_o = tf.zeros((batch_size,1)) #valor de x en condicion inicial x_0=0
-             y_o = self(x_o,training=True) #valor del modelo en en x_0
-             eq = dy + 2.*x*y_pred #Ecuacion diferencial evaluada en el modelo. Queremos que sea muy pequeno
-             ic = 1. #valor que queremos para la condicion inicial o el modelo en x_0
-             loss = self.mse(0., eq) + self.mse(y_o,ic)
+             dy = tape2.gradient(y_pred, x) # derivada del modelo con respecto a entradas x
+             x_o = tf.zeros((batch_size,1)) # valor de x en condicion inicial x_0=0
+             y_o = self(x_o,training=True) # valor del modelo en en x_0
+             eq = dy + 2.*x*y_pred # Ecuacion diferencial evaluada en el modelo. Queremos que sea muy pequeno
+             ic = 1. # valor que queremos para la condicion inicial o el modelo en x_0
+             loss = self.mse(0., eq) + self.mse(y_o,ic) # calculo de la función de pérdida
 
         # Apply grads
          grads = tape.gradient(loss, self.trainable_variables)
          self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
-        #update metrics
+        # update metrics
          self.loss_tracker.update_state(loss)
         # Return a dict mapping metric names to current value
          return {"loss": self.loss_tracker.result()}
