c6ff1fdc2dacbe8e8e05316fcc63ee2d2a48b18d
Paola de Truchis <paola.truchis@gmail.com>
2023-11-22 19:40:05 -0600

    añadi un documento : 'Part4' para hacer la pregunta 4 de la tarea

---
+++ EDP/Part4.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Input, Reshape
+from tensorflow.keras.optimizers import RMSprop, Adam
+
+from matplotlib import pyplot as plt
+import numpy as np
