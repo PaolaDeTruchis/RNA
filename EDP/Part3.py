c0cdaa3c23a70bd7cb41e53684b6c8346ce90b16
Paola de Truchis <paola.truchis@gmail.com>
2023-11-22 19:19:20 -0600

    añadi lineas de codigo para graficar la prediccion del modelo y la funcion pero na funciona

--- EDP/Part3.py
+++ EDP/Part3.py
@@ -20,12 +20,34 @@ class PolynomialLayer(Sequential):
     def calcular(self,x):
         return self.a0 + self.a1 * x + self.a2 * x**2 + self.a3 * x**3
     
+# Definicion de las funciones que tenemos que aproximar
+def function_a (x) : 
+    return np.cos(2 * x)
 
 # creacion de los datos de entrenamiento en el intervalo [-1,1]
 x_train = np.random.uniform(-1, 1, 1000)
 y_train = np.cos(2 * x_train)
 
-# Creacion del modelo con 
+# Creacion del modelo Sequential personalisado con PolynomialLayer 
 model = tf.keras.Sequential([
     PolynomialLayer()
 ])
+
+# compilacion del modélo
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# entrenamiento del modelo con la funcion cos(2x)
+model.fit(x_train, y_train, epochs=100, verbose=0)
+
+# Generando datos para evaluación en el intervalo [-1, 1]
+x_eval = np.linspace(-1, 1, 1000)
+y_pred = model.predict(x_eval)
+
+# Trazar gráficas para la función b
+plt.figure(figsize=(8, 6))
+plt.plot(x_eval, y_pred, color='red', label='Predictions')
+plt.plot(x_eval, function(x_eval), color='green', linestyle='--', label='True function')
+plt.title('Approximation de la fonction a')
+plt.legend()
+plt.show()
+
