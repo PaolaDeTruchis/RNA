f32443a249164d53bd44fa9d5ef6137099466e76
Paola de Truchis <paola.truchis@gmail.com>
2023-12-03 13:13:54 -0600

    Cree una 'class' para transformar la fotos a color en escala de grises

--- EDP/Part1.py
+++ EDP/Part1.py
@@ -4,5 +4,12 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout,Activation
 from tensorflow.keras.optimizers import RMSprop, Adam
 
-from matplotlib import pyplot as plt
-import numpy as np
+
+class ConvertToGrayscale(tf.keras.layers.Layer):
+    def __init__(self):
+        super(ConvertToGrayscale, self).__init__()
+
+    def call(self, inputs):
+        # Convertir la imagen a escala de grises
+        grayscale = tf.image.rgb_to_grayscale(inputs)
+        return grayscale
