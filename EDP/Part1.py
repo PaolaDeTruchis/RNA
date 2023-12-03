ae0e06b10d38570551dad85970829d8d5d5befe0
Paola de Truchis <paola.truchis@gmail.com>
2023-12-03 13:35:17 -0600

    Normalize los datos para poder entrenar la red

--- EDP/Part1.py
+++ EDP/Part1.py
@@ -13,3 +13,17 @@ class ConvertToGrayscale(tf.keras.layers.Layer):
         # Convertir la imagen a escala de grises
         grayscale = tf.image.rgb_to_grayscale(inputs)
         return grayscale
+    
+# Ejemplo coon la base de datos MINST 
+
+# Primero tenemos que cargar los dotas MINST
+(train_images, train_labels), _ = tf.keras.datasets.mnist.load_data()
+
+# Haora para que la red se antrena mas facilamente vamos a normalizar los datos.
+# Para representar las fotos, cada pixel tiene un numero (entre 0 y 255) que craracterisa
+# la nuancia de gris del mas obscuro (0 es negro) hasta el mas claro (255 es blanco).
+
+train_images = train_images / 255.0   # Asi todos los valores son entre 0 y 1
+
+
+
